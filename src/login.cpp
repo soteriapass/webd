368cf35441c1fc6a22df905280d6c37fcfff4276
viperman1271 <viperman1271@hotmail.com>
2017-06-04 12:05:37 -0400

    Basic login functionality is now working with the login page

--- src/login.cpp
+++ src/login.cpp
@@ -4,30 +4,25 @@
 LoginPage::LoginPage(const Net::Http::Request& request)
 : m_Authenticated(false)
 , m_Need2fa(false)
+, m_Conf("/etc/pswmgr/pswmgr.conf")
 , m_Client(m_Conf, PasswordManagerClient::GetChannel(m_Conf, m_Conf.get_authentication_address_and_port()))
 {
     if(request.method() == Net::Http::Method::Post)
     {
-        std::cout << request.body() << std::endl;
         auto splitStrings = split_string(request.body(), '&');
         for(const std::string& splitString : splitStrings)
         {
             auto querySplit = split_string(splitString, '=');
-            std::cout << querySplit[0] << ":" << querySplit[1] << std::endl;
             if(querySplit[0] == "username")
             {
-                std::cout << "username set" << std::endl;
                 m_Username = querySplit[1];
             }
             else if(querySplit[0] == "password")
             {
-                std::cout << "password set" << std::endl;
                 m_Password = unescape_text(querySplit[1]);
-                std::cout << "Password: " << m_Password << std::endl;
             }
             else if(querySplit[0] == "token")
             {
-                std::cout << "token set" << std::endl;
                 m_Token = querySplit[1];
             }
         }
@@ -58,7 +53,7 @@ void LoginPage::HeaderContent(std::stringstream& ss)
 {
     if(m_Authenticated)
     {
-        ss << "<meta http-equiv=\"refresh\" content=\"0; url=\"/?action=display\"/>" << std::endl;
+        ss << "<meta http-equiv=\"refresh\" content=\"2; URL=/?action=display\">" << std::endl;
     }
 }
 
@@ -70,30 +65,39 @@ void LoginPage::Page(std::stringstream& ss)
         std::string token;
     }
 
-    ss << "<h1>Login</h1>" << std::endl;
-    if(!m_Authenticated && !m_Client.GetLastError().empty())
+    if(!m_Authenticated)
     {
-        ss << "<font color=\"red\">" << m_Client.GetLastError() << "</font><br/>" << std::endl;
-    }
-    ss << "<form method=\"post\" action=\"/?action=login\">" << std::endl;
-    if(!m_Username.empty())
-    {
-        ss << "    Username: <input type=\"text\" name=\"username\" value=\"" << m_Username << "\">" << std::endl;
+        std::cout << "Not Authenticated" << std::endl;
+        ss << "<h1>Login</h1>" << std::endl;
+        if(!m_Client.GetLastError().empty())
+        {
+            ss << "<font color=\"red\">" << m_Client.GetLastError() << "</font><br/>" << std::endl;
+        }
+        ss << "<form method=\"post\" action=\"/?action=login\">" << std::endl;
+        if(!m_Username.empty())
+        {
+            ss << "    Username: <input type=\"text\" name=\"username\" value=\"" << m_Username << "\">" << std::endl;
+        }
+        else
+        {
+            ss << "    Username: <input type=\"text\" name=\"username\">" << std::endl;
+        }
+        ss << "    <p>" << std::endl;
+        ss << "    Password: <input type=\"password\" name=\"password\">" << std::endl;
+        ss << "    <p>" << std::endl;
+        if(m_Need2fa)
+        {
+            ss << "    2FA: <input type=\"text\" name=\"token\">" << std::endl;
+            ss << "    <p>" << std::endl;
+        }
+        ss << "    <input type=\"submit\" value=\"Login\">" << std::endl;
+        ss << "</form>" << std::endl;
     }
     else
     {
-        ss << "    Username: <input type=\"text\" name=\"username\">" << std::endl;
+        ss << "<h1>Authenticated succeeded</h1>" << std::endl;
+        ss << "If your browser doesn't automatically redirect you, please click <a href=\"/?action=display\">here</a>." << std::endl;
     }
-    ss << "    <p>" << std::endl;
-    ss << "    Password: <input type=\"password\" name=\"password\">" << std::endl;
-    ss << "    <p>" << std::endl;
-    if(m_Need2fa)
-    {
-        ss << "    2FA: <input type=\"text\" name=\"token\">" << std::endl;
-        ss << "    <p>" << std::endl;
-    }
-    ss << "    <input type=\"submit\" value=\"Login\">" << std::endl;
-    ss << "</form>" << std::endl;
 }
 
 bool LoginPage::DoLogin()
@@ -105,10 +109,6 @@ bool LoginPage::DoLogin()
     }
 
     m_Authenticated = m_Client.Authenticate(m_Username, m_Password, m_Token, m_Need2fa, false);
-    if(!m_Authenticated)
-    {
-        std::cout << "Authentication failed (" << m_Username << ", " << m_Password << ")" << std::endl;
-    }
     return m_Authenticated;
 }
 
