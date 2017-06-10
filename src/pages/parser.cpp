c720f995f460635327a1ff95e172acd1774f036f
viperman1271 <viperman1271@hotmail.com>
2017-06-10 12:05:52 -0400

    Supporting parsing in required files * Updated a bunch of variables in the body_start to be functions

--- src/pages/parser.cpp
+++ src/pages/parser.cpp
@@ -8,6 +8,7 @@ void Parser::Parse(const std::string& filename, std::stringstream& ss)
     std::string fileContents = std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()); 
     int lastPos = 0;
     int startPos = fileContents.find('$', lastPos);
+    const int firstStartPos = startPos;
     while(startPos != std::string::npos)
     {
         ss << fileContents.substr(lastPos, startPos - lastPos);
@@ -17,7 +18,6 @@ void Parser::Parse(const std::string& filename, std::stringstream& ss)
         int paramStart = fileContents.find('(', startPos + 1);
         std::string function = fileContents.substr(startPos + 1, paramStart - startPos - 1);
         std::string param = fileContents.substr(paramStart + 2, lastPos - paramStart - 5);
-
         if(function == "require")
         {
             Require(param, ss);
@@ -28,6 +28,10 @@ void Parser::Parse(const std::string& filename, std::stringstream& ss)
         }
 
         startPos = fileContents.find('$', lastPos + 2);
+        if(startPos == firstStartPos)
+        {
+            break;
+        }
     };
 
     if(lastPos != fileContents.size())
@@ -43,7 +47,17 @@ void Parser::RegisterParser(const std::string& name, std::function<void(const st
 
 void Parser::Require(const std::string& filename, std::stringstream& ss)
 {
-    std::ifstream file("static/" + filename, std::ifstream::in);
-    std::string fileContents = std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
-    ss << fileContents;
+    const std::string resolvedFilename = "static/" + filename;
+    int extIndexStart = resolvedFilename.rfind('.');
+    std::string ext = resolvedFilename.substr(extIndexStart + 1, extIndexStart - 1);
+    if(ext == "cshtml")
+    {
+        Parse(resolvedFilename, ss);
+    }
+    else
+    {
+        std::ifstream file("static/" + filename, std::ifstream::in);
+        std::string fileContents = std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
+        ss << fileContents;
+    }
 }
